dd5e980c463ddbf879197841b47fbd7893437a63
aalexjohn <alex@enlinx.com>
2021-05-20 08:57:15 -0600

    call gpio in function so that cleanup can occure at each run

--- hornControler.js
+++ hornControler.js
@@ -1,15 +1,16 @@
 var Gpio = require('onoff').Gpio;
-var LED = new Gpio(17, 'out');
+
 //var args = process.argv[2]
 
 //console.log('Horn on for '+args+'ms');
-LED.writeSync(1);
+
 
 module.exports.horn = (req, res)=>{
 
   let time = req.params.time1
 
     function fire(){
+            var LED = new Gpio(17, 'out');
             setImmediate(() => {
             LED.writeSync(0) //Turn on LED
             });
@@ -20,7 +21,7 @@ module.exports.horn = (req, res)=>{
     setTimeout(() => {
             console.log('Done', time);
             LED.writeSync(1); // Turn LED off
-        //    LED.unexport(); // Unexport GPIO to free resources
+            LED.unexport(); // Unexport GPIO to free resources
       }, time);
       res.status(200).json({status: "success", message: "success", time});
     
