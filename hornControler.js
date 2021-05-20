eeee758c909709b9cdf6a38ad70bcfefaa4859df
aalexjohn <alex@enlinx.com>
2021-05-20 17:14:38 -0600

    favicon!

--- hornControler.js
+++ hornControler.js
@@ -7,7 +7,7 @@
 
 module.exports.horn = (req, res)=>{
         
-        var validate = req.params.time1
+        var validate = parseInt(req.params.time1);
         console.log(isNaN(validate));
         if (isNaN(validate) === true) {
           let time = req.params.time1
