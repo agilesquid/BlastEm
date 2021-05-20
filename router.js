c3c9ce5dc33a528bb336ef89dfc0065574ca0bee
aalexjohn <alex@enlinx.com>
2021-05-19 18:39:40 -0600

    fff

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const hornControler = require('./hornControler');
 
-router.get('/d/:time1', hornControler.horn);
+router.get('/:time1', hornControler.horn);
 
 
 module.exports = router;
