3e391656f5956414454f321dfeb71915b3d0aa03
aalexjohn <alex@enlinx.com>
2021-05-20 09:07:43 -0600

    because

--- hornControler.js
+++ hornControler.js
@@ -8,13 +8,13 @@ var Gpio = require('onoff').Gpio;
 module.exports.horn = (req, res)=>{
 
   let time = req.params.time1
+  var LED = new Gpio(17, 'out');
 
     function fire(){
-            var LED = new Gpio(17, 'out');
             setImmediate(() => {
             LED.writeSync(0) //Turn on LED
             });
-    }
+    
     
     fire();
    // LED.writeSync(0);
@@ -24,5 +24,5 @@ module.exports.horn = (req, res)=>{
             LED.unexport(); // Unexport GPIO to free resources
       }, time);
       res.status(200).json({status: "success", message: "success", time});
-    
+}
 }
