641696d5ee4879b3083222a1011f5a1553ce596a
aalexjohn <alex@enlinx.com>
2021-05-20 22:08:39 -0600

    favicon!

--- hornControler.js
+++ hornControler.js
@@ -1,42 +1,28 @@
 
-
-//var args = process.argv[2]
-
-//console.log('Horn on for '+args+'ms');
-//LED.writeSync(1);
-
 module.exports.horn = (req, res)=>{
         if (req.params.time1 != "favicon.ico"){
-        var validate = [parseInt(req.params.time1)];
-        var time;
-         if (isNaN(validate[0]) == false) {
-                console.log(req.params.time1);
+                var validate = parseInt(req.params.time1);
+                var Gpio = require('onoff').Gpio;
+                var LED = new Gpio(17, 'out');
+                var time;
 
-                time = parseInt(req.params.time1);
-               
-        }else if (isNaN(validate[0]) == true){
-                console.log(req.params.time1);
-                time = 500
-        };
+                        if (isNaN(validate) == false) {
+                                time = parseInt(req.params.time1);
 
-        var Gpio = require('onoff').Gpio;
-        var LED = new Gpio(17, 'out');
-        
-        console.log(time,' ', validate[0]);
-        LED.writeSync(0); //Turn on LED
-  
+                        }else if (isNaN(validate) == true){
+                                time = 500
+                        };
 
-   // LED.writeSync(0);
-    setTimeout(() => {
-            console.log('Done', time);
-            LED.writeSync(1); // Turn LED off
-            LED.unexport(); // Unexport GPIO to free resources
-      }, time);
-      res.status(200).json({status: "success", message: "success"});
+                LED.writeSync(0); //Turn on LED
 
+                        setTimeout(() => {
+                                LED.writeSync(1); // Turn LED off
+                                LED.unexport(); // Unexport GPIO to free resources
+                        }, time);
+      
+                res.status(200).json({status: "success", message: "success"});
 
-}else{
-        res.status(200).json({status: "error", message: "error"});
-}
-
+        }else{
+                res.status(200).json({status: "", message: ""});
+        } 
 }
