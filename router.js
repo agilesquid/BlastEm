6e208ebd8817b05a0ec4a42123178233d90ce13e
aalexjohn <alex@enlinx.com>
2021-05-21 14:52:28 -0600

    chachachachacha CHANGES!

--- router.js
+++ router.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const hornControler = require('./hornControler');
-const app = express();
-
-
-app.use(express.static('Public'));
 
 router.get('/:time1', hornControler.horn);
 
--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const port = 80;
 const router = require('./router');
 
 
-
-
 app.use('/', router);
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
