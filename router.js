8816288dbc3656ba053c4b0ceb3458016e44e586
aalexjohn <alex@enlinx.com>
2021-05-20 21:30:35 -0600

    favicon!

--- router.js
+++ router.js
@@ -2,19 +2,7 @@ const express = require('express');
 const router = express.Router();
 const hornControler = require('./hornControler');
 
-const http = require('http');
 
-
-const server = http.createServer(function(req, res) {
-        if(req.url === '/favicon.ico') {
-            console.log('ignore');
-        } else {
-            console.log(req.url);
-        res.end();            
-    }
-});
-
-server.listen(80);
-router.get('/:time1', hornControler.horn);
+    router.get('/:time1', hornControler.horn);
 
 module.exports = router;
