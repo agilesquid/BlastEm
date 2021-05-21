dbf201afc6f757628a8d50fc9dd3dec9d82a30a7
aalexjohn <alex@enlinx.com>
2021-05-21 11:40:57 -0600

    favicon!

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const hornControler = require('./hornControler');
+const hornControler = require('./hornControlerTest');
 const app = express();
 
 
