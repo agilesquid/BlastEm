f37258d5e5b39997d5f721d7bce59f68dafbe0bb
aalexjohn <alex@enlinx.com>
2021-05-24 13:38:39 -0600

    -

--- hornControlerTest.js
+++ hornControlerTest.js
@@ -7,12 +7,12 @@ module.exports.horn = (req, res)=>{
         var Gpio = require('onoff').Gpio;
         var LED = new Gpio(4, 'out'); //change to 17
         //let LEDStatus = LED.readSync();
-        let Waiting = false;
         console.log("Wait? " , Waiting);
         if (Waiting == true){
             return res.status(400).json({status: "busy", message: "please wait for current process to complete"});
         }
-                Waiting = true;
+        
+               let Waiting = true;
                 var validate = parseInt(req.params.time1);
                 var time;
                         if (isNaN(validate) == false) {
