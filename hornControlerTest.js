ab25edc187604edffe0792d2a893c532c5417f47
aalexjohn <alex@enlinx.com>
2021-05-24 14:03:19 -0600

    -

--- hornControlerTest.js
+++ hornControlerTest.js
@@ -1,35 +1,23 @@
 
 module.exports.horn = (req, res)=>{
-    console.log('this is a test');
-        if (req.params.time1 == "favicon.ico"){
-                return res.status(400).json({status: "Error", message: "Error"});
-        }
+    if (req.params.time1 == "favicon.ico"){
+            return res.status(400).json({status: "Error", message: "Error"});
+    }
         var Gpio = require('onoff').Gpio;
         var LED = new Gpio(4, 'out'); //change to 17
-        //let LEDStatus = LED.readSync();
-        console.log("Wait? " , Waiting);
-        if (Waiting == true){
-            return res.status(400).json({status: "busy", message: "please wait for current process to complete"});
-        }
-        
-               let Waiting = true;
-                var validate = parseInt(req.params.time1);
-                var time;
-                        if (isNaN(validate) == false) {
-                                time = parseInt(req.params.time1);
-
-                        }else if (isNaN(validate) == true){
-                                time = 500
-                        };
-                        console.log("Wait? " , Waiting);
-                console.log('Horn on for ', time, 'ms');
-                LED.writeSync(0); //Turn on LED
-
-                        setTimeout(() => {
-                                LED.writeSync(1); // Turn LED off
-                                LED.unexport(); // Unexport GPIO to free resources
-                            }, time);
-                            
-                            res.status(200).json({status: "success", message: "Horn on for " + time + "ms"});
-                            Waiting = false;
+        var validate = parseInt(req.params.time1);
+        var time;
+                if (isNaN(validate) == false) {
+                        time = parseInt(req.params.time1);
+                }else if (isNaN(validate) == true){
+                        time = 500
+                };
+            console.log('Horn on for ', time, 'ms');
+            LED.writeSync(0); //Turn on LED
+                    setTimeout(() => {
+                            LED.writeSync(1); // Turn LED off
+                            LED.unexport(); // Unexport GPIO to free resources
+                        }, time);
+                        
+                        res.status(200).json({status: "success", message: "Horn on for " + time + "ms"});
 }
